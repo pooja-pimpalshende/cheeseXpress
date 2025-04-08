4d755974486895119801e2a3f4e1680a8afea856
Pooja <pooja.pimpalshende215@gmail.com>
2025-04-08 17:16:50 +0200

    eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
@@ -17,18 +18,22 @@ export default [
       },
     },
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "no-unused-vars": ["warn", { varsIgnorePattern: "^[A-Z_]" }],
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
       "react/react-in-jsx-scope": "off",
+      // "no-unused-vars": ["warn", { varsIgnorePattern: "^_" }],
+      "react/no-unescaped-entities": "off",
     },
   },
 ];

9c006edb2a13d450ff10579718f5e6782ab9b391
Pooja <pooja.pimpalshende215@gmail.com>
2025-04-15 15:43:27 +0200

    Added flex styles

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Pooja</div>;
+  return <div className="hidden text-sm font-semibold md:block">Pooja</div>;
 }
 
 export default Username;

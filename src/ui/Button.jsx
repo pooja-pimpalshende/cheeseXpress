b103a1841816fbe1cd134c35a4a0d5af700575d3
Pooja <pooja.pimpalshende215@gmail.com>
2025-04-17 15:50:47 +0200

    Aded favicon, styled order page

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,13 +3,13 @@ import { Link } from '@tanstack/react-router';
 /* eslint-disable react/prop-types */
 function Button({ children, disabled, to, type }) {
   const base =
-    'inline-block rounded-full bg-yellow-500 uppercase font-semibold tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-400 focus:bg-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed';
+    'inline-block text-sm rounded-full bg-yellow-500 uppercase font-semibold tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-400 focus:bg-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed';
 
   const styles = {
     primary: base + ' px-3 py-2 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-sm',
     secondary:
-      'inline-block rounded-full border-2 border-stone-300 uppercase font-semibold tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed px-3 py-2.5 md:px-6 md:py-3.5',
+      'inline-block text-sm rounded-full border-2 border-stone-300 uppercase font-semibold tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed px-3 py-2.5 md:px-6 md:py-3.5',
   };
 
   if (to) {

8ef1bec97b6f7760f26e0f1e49227b25510d1c5a
Pooja <pooja.pimpalshende215@gmail.com>
2025-04-17 16:54:25 +0200

    created user store, user state with redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-constant-binary-expression */
 import { Outlet } from '@tanstack/react-router';
 import Header from './ui/Header';
 import CartOverview from './features/cart/CartOverview';
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from './router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider } from 'react-redux';
+import store from './store.js';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,11 +17,13 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
+  </StrictMode>,
 );
